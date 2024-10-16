253568204b1776d60b3e555dd5d5da7a6d4f689e
Lucas Martins <lukasousa70@gmail.com>
2024-10-16 15:10:13 -0300

    first commit

--- assets/js/listaTarefas.js
+++ assets/js/listaTarefas.js
@@ -7,10 +7,38 @@ function criaLi(){
     return li;
 }
 
+// Capturar o evento de pressionar o botão (Enter)
+inputTarefa.addEventListener('keypress', function(e){
+    // console.log(e); // Visualize no console web o botão e a keycode
+     if (e.keyCode === 13){
+         if (!inputTarefa.value) return; // Se o retorno no botao for vazio 
+         criaTask(inputTarefa.value);
+        }
+ });
+
+ function limpaInput () {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+// Função para criar o botão de apagar 
+function criaBotaoApagar (li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar'; 
+    //botaoApagar.classList.add('Apagar');
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+    li.appendChild(botaoApagar);
+}
+
 function criaTask (textInput) {
-    const l = criaLi();
-    l.innerText = textInput;
-    tarefas.appendChild(l); // Para add a task com o botao 
+    const li = criaLi();
+    li.innerText = textInput;
+    tarefas.appendChild(li); // Para add a task com o botao
+    limpaInput();
+    criaBotaoApagar();
+    salvarTarefas(); 
 }
 
 // Capturar o click no botão de add a task 
